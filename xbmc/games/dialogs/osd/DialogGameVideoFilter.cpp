d914a4d6e436bd8b0d3fffbf410fda3c045e951d
Kostas Andrianos <gusandrianos@gmail.com>
2019-09-29 13:18:12 +0300

    Change xml selection from platform-specific tag to file

--- xbmc/games/dialogs/osd/DialogGameVideoFilter.cpp
+++ xbmc/games/dialogs/osd/DialogGameVideoFilter.cpp
@@ -99,9 +99,16 @@ void CDialogGameVideoFilter::InitVideoFilters()
 {
   std::vector<VideoFilterProperties> videoFilters;
 
+  std::string xmlFilename;
+#ifdef TARGET_WINDOWS
+  xmlFilename = "ShaderPresetsHLSLP.xml";
+#else
+  xmlFilename = "ShaderPresetsGLSLP.xml";
+#endif
+
   // TODO: Have the add-on give us the xml as a string (or parse it)
   static const std::string addonPath = std::string("special://xbmcbinaddons/") + PRESETS_ADDON_NAME;
-  static const std::string xmlPath = addonPath + "/resources/ShaderPresetsDefault.xml";
+  static const std::string xmlPath = addonPath + "/resources/" + xmlFilename;
   std::string basePath = URIUtils::GetBasePath(xmlPath);
 
   CXBMCTinyXML xml = CXBMCTinyXML(xmlPath);
@@ -123,11 +130,7 @@ void CDialogGameVideoFilter::InitVideoFilters()
       continue;
 
     TiXmlNode* pathNode;
-#ifdef TARGET_WINDOWS
-    if ((pathNode = child->FirstChild("hlslp-path")))
-#else
-    if ((pathNode = child->FirstChild("glslp-path")))
-#endif
+    if ((pathNode = child->FirstChild("path")))
       if ((pathNode = pathNode->FirstChild()))
         videoFilter.path = URIUtils::AddFileToFolder(basePath, pathNode->Value());
     TiXmlNode* nameIndexNode;
