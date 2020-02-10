919b00b3b626ff8090baeb2167fa634eb78a948f
Lukas Rusak <lorusak@gmail.com>
2020-02-10 15:13:43 -0800

    CDateTime: convert to use std::chrono

--- xbmc/pvr/guilib/PVRGUIActions.cpp
+++ xbmc/pvr/guilib/PVRGUIActions.cpp
@@ -1732,8 +1732,6 @@ namespace PVR
         return false;
     }
 
-    CDateTime::ResetTimezoneBias();
-
     CLog::LogFC(LOGDEBUG, LOGPVR, "PVR clearing %s database", bResetEPGOnly ? "EPG" : "PVR and EPG");
 
     pDlgProgress->SetHeading(CVariant{313}); // "Cleaning database"
